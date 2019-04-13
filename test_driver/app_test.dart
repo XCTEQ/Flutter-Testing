a01d71ba990558840c2e286bbcf3c87f04b5d73c
Shashikant86 <shashikant.jagtap@icloud.com>
2019-04-13 09:45:46 +0100

    Added integration tests

---
+++ test_driver/app_test.dart
@@ -0,0 +1,28 @@
+// Imports the Flutter Driver API
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Test Launch App', () {
+    FlutterDriver driver;
+
+    setUpAll(() async {
+      // Connects to the app
+      driver = await FlutterDriver.connect();
+    });
+
+    tearDownAll(() async {
+      if (driver != null) {
+        // Closes the connection
+        driver.close();
+      }
+    });
+
+    test('launch', () async {
+
+      SerializableFinder message = find.text("You have pushed the button this many times:");
+      await driver.waitFor(message); 
+      expect(await driver.getText(message), "You have pushed the button this many times:");         
+    });
+  });
+}
