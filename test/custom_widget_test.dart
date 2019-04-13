c29e7a5e97df3780aadd42f50000264e320d1c57
Shashikant86 <shashikant.jagtap@icloud.com>
2019-04-13 08:50:41 +0100

    Added another widget test for enterring text

--- test/custom_widget_test.dart
+++ test/custom_widget_test.dart
@@ -56,7 +56,37 @@ testWidgets('Create and test my own widget', (WidgetTester tester) async {
     expect(messageFinder, findsOneWidget);
    });
 
+  // Third Test t enter text, tap 
+
+  testWidgets('Add and remove a todo', (WidgetTester tester) async {
+    // Build the Widget
+    await tester.pumpWidget(TodoList());
+
+    // Enter 'hi' into the TextField
+    await tester.enterText(find.byType(TextField), 'hi');
+
+    // Tap the add button
+    await tester.tap(find.byType(FloatingActionButton));
+
+    // Rebuild the Widget with the new item
+    await tester.pump();
+
+    // Expect to find the item on screen
+    expect(find.text('hi'), findsOneWidget);
+
+    // Swipe the item to dismiss it
+    await tester.drag(find.byType(Dismissible), Offset(500.0, 0.0));
+
+    // Build the Widget until the dismiss animation ends
+    await tester.pumpAndSettle();
+
+    // Ensure the item is no longer on screen
+    expect(find.text('hi'), findsNothing);
+  });
+
 }
+
+// Widget for second test 
 
 class MyWidget extends StatelessWidget {
   final String title;
@@ -78,6 +108,62 @@ class MyWidget extends StatelessWidget {
         ),
         body: Center(
           child: Text(message),
+        ),
+      ),
+    );
+  }
+}
+
+// Widget for the third test
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  static const _appTitle = 'Todo List';
+  final todos = <String>[];
+  final controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(_appTitle),
+        ),
+        body: Column(
+          children: [
+            TextField(
+              controller: controller,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: todos.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final todo = todos[index];
+
+                  return Dismissible(
+                    key: Key('$todo$index'),
+                    onDismissed: (direction) => todos.removeAt(index),
+                    child: ListTile(title: Text(todo)),
+                    background: Container(color: Colors.red),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              todos.add(controller.text);
+              controller.clear();
+            });
+          },
+          child: Icon(Icons.add),
         ),
       ),
     );
